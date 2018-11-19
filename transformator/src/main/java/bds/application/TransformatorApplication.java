de8a73d01448215aac4036ce5f35243fcdbcfdb5
den3011den <den3011den@gmail.com>
2018-11-20 02:27:00 +0700

    Доработка проекта до состояния простого запуска / останова (консольное приложение)

---
+++ transformator/src/main/java/bds/application/TransformatorApplication.java
@@ -0,0 +1,28 @@
+package bds.application;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class TransformatorApplication implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(TransformatorApplication.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(TransformatorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        log.info("Application run");
+
+        log.info("Application stopped");
+
+        System.exit(0);
+
+    }
+}
